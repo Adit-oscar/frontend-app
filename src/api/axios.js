22eb4c5bdca80c3fb335d74d3a1e06da1e1e7293
Adit-oscar <tiyaa0048@gmail.com>
2026-07-24 09:36:46 +0800

    menambahkan interceptor saat akses api dengan pengecekan token sebelum mengakses / hit api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { config } from "dotenv";
 const URL_API = import.meta.env.VITE_URL_API;
 
 const api = axios.create({
@@ -8,5 +9,32 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (request) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      request.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return request;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      if (!window.location.pathname.startsWith("/auth")) {
+        window.location.href = "/auth/login";
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
 export default function Dashboard() {
   return (
-    <div className="h-full min-h-screen w-screen bg-blue-300 flex justify-center items-center">
+    <div className="min-h-screen w-screen bg-blue-300 flex justify-center items-center">
       <h4>Halaman Dashboard</h4>
     </div>
   );
